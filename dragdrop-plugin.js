f787c86772fbfae4294949a2e1dbd83e68546ff5
hanbinl1 <38491392+hanbinl1@users.noreply.github.com>
2020-05-10 00:00:03 +1000

    update plugin

---
+++ dragdrop-plugin.js
@@ -0,0 +1,84 @@
+/********************************************************************/
+
+define(function () {
+
+    return {
+
+        name: "Drag and Drop file",
+        author: "Hanbin Li",
+        email: "hanbinl1@student.unimelb.edu.au",
+        description: "Allow users to drag and drop file on the left pane.",
+
+        initialize: function() {
+
+        	 // Check for the various File API support.
+		    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+		        window.toastr.error('The File APIs are not fully supported by your browser.');
+		        return;
+		    }
+
+		    var dropRegion = document.getElementById("editor-tabs");
+
+
+			dropRegion.addEventListener('dragenter', preventDefault, false);
+			dropRegion.addEventListener('dragleave', preventDefault, false);
+			dropRegion.addEventListener('dragover', preventDefault, false);
+			dropRegion.addEventListener('drop', handleDrop, false);
+
+
+
+			function preventDefault(e) {
+			    e.preventDefault();
+			    e.stopPropagation();
+			}
+
+
+
+			function handleDrop(e) {
+			    e.stopPropagation();
+			    e.preventDefault();
+
+			    var files = e.dataTransfer.files;
+
+			    for (var i = 0, len = files.length; i < len; i++) {
+			    	if (files[i]) {
+			      		previewAnduploadFile(files[i]);
+			    	} else {
+			      		window.toastr.error("Failed to load file");
+			    	}
+			 	}
+
+			}
+
+
+			function previewAnduploadFile(file) {
+			    var r = new FileReader();
+			    r.onload = function(e) {
+			        var contents = e.target.result;
+			        createEditor();
+			        $('#tab-' + window.current_editor).text(file.name);
+			        var editor = window.ace.edit(window.current_editor);
+			        editor.getSession().setValue(contents);
+			    }
+			    r.readAsText(file);
+			}
+
+        },
+
+
+
+        disable: function() { /* Do Nothing */ },
+
+        save: function() {
+        	return {};
+        },
+
+        load: function(settings) {
+
+        }
+
+    };
+});
+
+
+
