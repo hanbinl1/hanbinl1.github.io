01ee44edb234037ccb529b60f66c03ec2367d457
hanbinl1 <38491392+hanbinl1@users.noreply.github.com>
2020-05-16 04:38:45 +1000

    reformatted

--- dragdrop-plugin.js
+++ dragdrop-plugin.js
@@ -10,81 +10,78 @@ define(function () {
         description: "Allow users to drag and drop file on the main window.",
 
         initialize: function() {
+            // Check for the various File API support.
+	    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+		window.toastr.error('The File APIs are not fully supported by your browser.');
+		return;
+	    }
+
+	    var dropRegion = document.getElementById("editors");
 
 
-        	 // Check for the various File API support.
-		    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
-		        window.toastr.error('The File APIs are not fully supported by your browser.');
-		        return;
+	    dropRegion.addEventListener('dragenter', preventDefault, false);
+	    dropRegion.addEventListener('dragleave', preventDefault, false);
+	    dropRegion.addEventListener('dragover', preventDefault, false);
+	    dropRegion.addEventListener('drop', handleDrop, false);
+
+
+
+	    function preventDefault(e) {
+		e.preventDefault();
+		e.stopPropagation();
+	    }
+
+
+
+	    function handleDrop(e) {
+		e.stopPropagation();
+		e.preventDefault();
+
+	        var files = e.dataTransfer.files;
+
+		for (var i = 0, len = files.length; i < len; i++) {
+		    if (files[i]) {
+		        previewAnduploadFile(files[i]);
+		    } else {
+	     	        window.toastr.error("Failed to load file");
 		    }
-
-		    var dropRegion = document.getElementById("editors");
-
-
-			dropRegion.addEventListener('dragenter', preventDefault, false);
-			dropRegion.addEventListener('dragleave', preventDefault, false);
-			dropRegion.addEventListener('dragover', preventDefault, false);
-			dropRegion.addEventListener('drop', handleDrop, false);
+		}   
+	    }  
 
 
-
-			function preventDefault(e) {
-			    e.preventDefault();
-			    e.stopPropagation();
-			}
-
-
-
-			function handleDrop(e) {
-			    e.stopPropagation();
-			    e.preventDefault();
-
-			    var files = e.dataTransfer.files;
-
-			    for (var i = 0, len = files.length; i < len; i++) {
-			    	if (files[i]) {
-			      		previewAnduploadFile(files[i]);
-			    	} else {
-			      		window.toastr.error("Failed to load file");
-			    	}
-			 	}
-
-			}
-
-
-			function previewAnduploadFile(file) {
-			    var r = new FileReader();
-			    r.onload = function(e) {
-			        var contents = e.target.result;
-			        createEditor();
-			        $('#tab-' + window.current_editor).text(file.name);
-			        var editor = window.ace.edit(window.current_editor);
-			        editor.getSession().setValue(contents);
-			    }
-			    r.readAsText(file);
-			}
+	    function previewAnduploadFile(file) {
+		var r = new FileReader();
+	        r.onload = function(e) {
+		    var contents = e.target.result;
+		    createEditor();
+		    $('#tab-' + window.current_editor).text(file.name);
+		    var editor = window.ace.edit(window.current_editor);
+		    editor.getSession().setValue(contents);
+                }
+	        r.readAsText(file);
+	    }
 
         },
 
 
 
         disable: function() { 
-			var dropRegion = document.getElementById("editors");
+	    var dropRegion = document.getElementById("editors");
 
-			dropRegion.addEventListener('dragenter', preventDefault, false);
-			dropRegion.addEventListener('dragleave', preventDefault, false);
-			dropRegion.addEventListener('dragover', preventDefault, false);
-			dropRegion.addEventListener('drop', preventDefault, false);
+	    dropRegion.addEventListener('dragenter', preventDefault, false);
+	    dropRegion.addEventListener('dragleave', preventDefault, false);
+	    dropRegion.addEventListener('dragover', preventDefault, false);
+	    dropRegion.addEventListener('drop', preventDefault, false);
 			
-			function preventDefault(e) {
-			    e.preventDefault();
-			    e.stopPropagation();
-			}
+	    function preventDefault(e) {
+	        e.preventDefault();
+	        e.stopPropagation();
+	    }
 
-		},
+	},
 
         save: function() {
-        	return {};
+            return {};
         },
 
         load: function(settings) {
